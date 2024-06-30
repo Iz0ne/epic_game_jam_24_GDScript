791b70ee395204cc5bfe6ceb82bfae15bf47c5cc
unknown <matboardeurs@hotmail.com>
2024-06-30 14:22:47 +0200

    Add animation

--- Enigma/scenes/scripts/Player.gd
+++ Enigma/scenes/scripts/Player.gd
@@ -10,6 +10,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var xDirection = Input.get_axis("ui_left", "ui_right")
 	velocity.x = xDirection * SPEED
 	
